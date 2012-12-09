4d1fdaa0c1fc9843249626f684bc10683993036c
Alexandru Irimia <alexandru138@yahoo.com>
2012-12-09 17:04:09 +0200

    New things...or not

---
+++ api/put_cell.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+require('dbinfo.php');
+
+if (!$_SESSION['user']) exit();
+
+$userid = mysql_real_escape_string($_SESSION['user']['id']);
+$docid = mysql_real_escape_string($_POST['docid']);
+$cell_id = mysql_real_escape_string($_POST['cell_id']);
+$content = mysql_real_escape_string($_POST['content']);
+
+$response = array();
+
+$query = "select * from spreadsheets where (fk_userID ='$userid'
+         or id=(select fk_sheetID from sp_rights where fk_userID='$userid')) and id='$docid'";
+$result = mysql_query($query);
+$files = array();
+
+$query = "INSERT INTO sp_data (fk_sheetID, fk_userID, cell_id, content) VALUES ('$docid ', '$userid', '$cell_id', '$content')";
+$result = mysql_query($query);
+
+$response['status'] = 'OK';
+
+echo json_encode($response);
+
+?>
