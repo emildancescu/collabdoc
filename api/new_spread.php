daf2ff73691c11a55ef6cf7d99d877783cbecdbe
Alexandru Irimia <alexandru138@yahoo.com>
2012-12-09 15:46:14 +0200

    Stage 2

--- api/new_spread.php
+++ api/new_spread.php
@@ -5,26 +5,29 @@ require('dbinfo.php');
 if (!$_SESSION['user']) exit();
 
 $userid = mysql_real_escape_string($_SESSION['user']['name']);
-$spreadsheet_name = mysql_real_escape_string($_POST['name']);
+$docname = mysql_real_escape_string($_POST['name']);
 $response = array();
 
 if ($spreadsheet_name == "") {
 	$response['status'] = 'error';
 	$response['errors'] = array();
-	$response['errors']['speardsheet_name'] = "You must provide a name";
+	$response['errors']['docname'] = "You must provide a name";
+	echo json_encode($response);
+	exit();
 }
 
 //Check if user already has a spreadsheet with that name
-$query = "Select id from SPREADSHEETS where fk_userid = " + $userid + "and spreadname is '" + $spreadsheet_name + "'";
+$query = "Select id from SPREADSHEETS where fk_userid = " + $userid + "and spreadname is '" + $docname + "'";
 $result = mysql_query($query);
 if (mysql_num_rows($result)) {
 	$response['status'] = 'error';
 	$response['errors'] = array();
-	$response['errors']['speardsheet_name'] = "You already have a spreadsheet with that name";
+	$response['errors']['docname'] = "You already have a spreadsheet with that name";
+	echo json_encode($response);
+	exit();
 }
 
-
-$query = "INSERT INTO SPREADSHEETS ('fk_userid', 'spreadname') VALUES('" + $spreadsheet_name + " , " + $userid + "')";
+$query = "INSERT INTO SPREADSHEETS ('fk_userid', 'spreadname') VALUES('" + $docname + " , " + $userid + "')";
 $result = mysql_query($query);
 
 while ($row = mysql_fetch_assoc($result)) {
@@ -33,8 +36,7 @@ while ($row = mysql_fetch_assoc($result)) {
 
 $response['status'] = 'OK';
 $response['id'] = $id;
-$response['spreadsheet_name'] = $spreadsheet_name;
-
+$response['docname'] = $docname;
 
 echo json_encode($response);
 
