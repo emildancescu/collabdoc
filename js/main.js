4653bb050f0e335c93ea20e7a19b6b4932dc06a8
Emil <emil@Emil.local>
2012-12-09 17:58:07 +0200

    file variable fix

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@
 				var file = response.files[i];
 				
 				var html = '<td>'+i+'</td>';
-				html += '<td>'+files.docname+'</td>';
-				html += '<td>'+files.data+'</td>';
+				html += '<td>'+file.docname+'</td>';
+				html += '<td>'+file.data+'</td>';
 				html += '<td><a href="doc.html" target="_blank" class="btn btn-success">edit</a></td>';
 				
 				tr = $('<tr>').html(html);
