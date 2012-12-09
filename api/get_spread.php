0fc713b8b81d53f1cff040b469c7210f8834ff1f
Alexandru Irimia <alexandru138@yahoo.com>
2012-12-09 16:40:23 +0200

    Added get files

---
+++ api/get_spread.php
@@ -0,0 +1,35 @@
+<?php
+
+require('dbinfo.php');
+
+//if (!$_SESSION['user']) exit();
+//$userid = mysql_real_escape_string($_SESSION['user']['id']);
+$userid = $_GET['user'];
+
+$response = array();
+$response['status'] = 'OK';
+
+$query = "select * from spreadsheets where fk_userID ='$userid'
+         or id=(select fk_sheetID from sp_rights where fk_userID='$userid')
+		 ORDER BY data ASC";
+$result = mysql_query($query);
+$files = array();
+
+while ($row = mysql_fetch_assoc($result))
+{
+	$temp = array();
+	$temp['id'] = $row['id'];
+	$temp['docname'] = $row['docname'];
+	$temp['data'] = $row['data'];
+	if ($row['fk_userID'] == $userid)
+		$temp['owner'] = 'YES';
+	else
+		$temp['owner'] = 'NO';
+	$files[] = $temp;
+}
+
+$response['files'] = $files;
+
+echo json_encode($response);
+
+?>
